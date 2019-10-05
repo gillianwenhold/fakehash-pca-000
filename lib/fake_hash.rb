223ec49fc352b84ffc5642e7473046c70ed4fdf4
Gillian Wenhold <gwenhold@mac.com>
2019-10-04 22:31:53 -0400

    Done.

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,29 @@
 class FakeHashWrapper
+  attr_accessor :hash
 
-  # write code here
+  def initialize
+    @hash = {}
+  end
 
+  def []=(key, val)
+    @hash[key.to_sym] = val
+  end
+
+  def [](key)
+    @hash[key.to_sym]
+  end
+
+  def keys
+    @hash.keys.sort.reverse
+  end
+
+  def first_letter(letter)
+    @hash.values.select{|val| @hash.key(val)[0] == letter}
+  end
+
+  def each
+    @hash.each do |h|
+      yield(h)
+    end
+  end
 end
